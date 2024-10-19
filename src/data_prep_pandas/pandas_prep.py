510582b62cf58e5cea79b503ba26c60c49eb7de0
HackYou-Yang17 <74777817+HackYou-Yang17@users.noreply.github.com>
2024-10-19 22:01:37 +0100

    completed activity 2.05

--- src/data_prep_pandas/pandas_prep.py
+++ src/data_prep_pandas/pandas_prep.py
@@ -15,13 +15,14 @@ def print_dataframe_data(dataframe):
     
         """
 
-        print("\nData types of the start/end columns after conversion:")
-        print(dataframe.loc[:, ['start', 'end']].dtypes)
+        print (dataframe)
+        #print("\nData types of the start/end columns after conversion:")
+        #print(dataframe.loc[:, ['start', 'end']].dtypes)
 
-        print(dataframe.loc[:, ['start', 'end']])
+        #print(dataframe.loc[:, ['start', 'end']])
 
 
-def data_preparation(dataframe):
+def data_preparation(df1, df2):
     """Summary or Description of the Function
     
            Parameters:
@@ -32,32 +33,39 @@ def data_preparation(dataframe):
     
         """
     
-    dataframe['start'] = pd.to_datetime(dataframe['start'], format = '%d/%m/%Y')
-    dataframe['end'] = pd.to_datetime(dataframe['end'], format = '%d/%m/%Y')
+    df1['start'] = pd.to_datetime(df1['start'], format = '%d/%m/%Y')
+    df1['end'] = pd.to_datetime(df1['end'], format = '%d/%m/%Y')
     
+    '''
     try:
         columns_to_change= ['countries', 'events', 'participants_m', 'participants_f', 'participants']
 
         for x in columns_to_change:
-            dataframe[x] = dataframe[x].astype('int')
+            df1[x] = df1[x].astype('int')
     except ValueError as e:
-        print(f"Error, can't convert column {dataframe[x].name} to int: {e}")
+        print(f"Error, can't convert column {df1[x].name} to int: {e}") 
+    '''
 
+    return df1.merge(df2, how='left', left_on='country', right_on='Name')
+
+    
 
 if __name__ == '__main__':
 
     try:
         csvPath = pathlib.Path(__file__).parent.parent.joinpath('tutorialpkg' ,'data', 'paralympics_events_raw.csv')
         xlsxPath = pathlib.Path(__file__).parent.parent.joinpath('tutorialpkg' ,'data', 'paralympics_all_raw.xlsx')
+        npcPath = pathlib.Path(__file__).parent.parent.joinpath('tutorialpkg' ,'data', 'npc_codes.csv')
     except FileNotFoundError as e:
         print(f"File not found. Please check the file path. Error: {e}")
 
     csvDf = pd.read_csv(csvPath)
     xlsxDf1 = pd.read_excel(xlsxPath)
     xlsxDf2 = pd.read_excel(xlsxPath, sheet_name = "medal_standings")
-
-    data_preparation(xlsxDf1)
-    print_dataframe_data(xlsxDf1)
+    npcDf = pd.read_csv(npcPath, encoding='utf-8', encoding_errors='ignore', usecols=['Code', 'Name'])
+    
+    mergedDf = data_preparation(csvDf, npcDf)
+    print_dataframe_data(mergedDf)
 
     print("\nDone")
     
